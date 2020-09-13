c704d2ea9c1a8860a4a8c98ea58bd9b913053f89
roxgodoya <68455336+roxgodoya@users.noreply.github.com>
2020-09-13 18:47:21 -0300

    ultima version
    Se relizaron en local todas las modificaciones para la maqueta

--- static/js/main.js
+++ static/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' El modelo predijo que la imagen pertenece a la categoría:  ' + data);
+                $('#result').text(' La imagen que subió, correponde al deporte:  ' + data);
                 console.log('Success!');
             },
         });
